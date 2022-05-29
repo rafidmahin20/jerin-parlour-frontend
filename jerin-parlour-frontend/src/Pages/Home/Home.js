2ed61e9665cf203c67d73298b7a4991916d6445a
rafidmahin20 <submanmahin@gmail.com>
2022-05-29 13:35:04 +0600

    edited

---
+++ jerin-parlour-frontend/src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div className='px-12'>
+            <Helmet>
+                <title>Home - Jerin's Parlour</title>
+            </Helmet>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
